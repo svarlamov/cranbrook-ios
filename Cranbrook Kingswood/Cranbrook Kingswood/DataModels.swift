c2eb0a5ca12c3ca120fc76797cd77735402fd382
Stefan DeClerck <stefan.declerck26@gmail.com>
2016-08-06 23:24:55 -0700

    added structure for class list

--- Cranbrook Kingswood/Cranbrook Kingswood/DataModels.swift
+++ Cranbrook Kingswood/Cranbrook Kingswood/DataModels.swift
@@ -11,12 +11,84 @@ import ObjectMapper
 import Alamofire
 import SwiftyJSON
 
+// MARK: - structure for current student session info
 struct CurrentLoggedInUserInfo {
-    let userId: String
-    let sessionToken: String
+    var userId: String
+    var sessionToken: String
     
     init(userId: String, sessionToken: String) {
         self.userId = userId
         self.sessionToken = sessionToken
     }
 }
+
+
+// MARK: - structure for class list
+struct CurrentUserClasses: Mappable {
+    var albumId: Int?
+    var durationId: Int?
+    var filePath: String?
+    var ownerId: Int?
+    var assignmentActiveToday: Int?
+    var assignmentAssignedToday: Int?
+    var assignmentDueToday: Int?
+    var canViewAssignments: Bool?
+    var courseDescription: String?
+    var cumGrade: String?
+    var currentTerm: String?
+    var gradeBookCumGpa: Bool?
+    var groupOwnerEmail: String?
+    var groupOwnerName: String?
+    var groupOwnerPhoto: String?
+    var leadSectionId: Int?
+    var markingPeriodId: Int?
+    var mostRecentGroupPhoto: String?
+    var publishGroupToUser: Bool?
+    var room: String?
+    var schoolLevel: String?
+    var sectionId: Int?
+    var sectionIdentifier: String?
+    
+    init?(_ map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        albumId <- map["AlbumId"]
+        durationId <- map["DurationId"]
+        filePath <- map["FilePath"]
+        ownerId <- map["OwnerId"]
+        assignmentActiveToday <- map["assignmentactivetoday"]
+        assignmentAssignedToday <- map["assignmentassignedtoday"]
+        assignmentDueToday <- map["assignmentduetoday"]
+        canViewAssignments <- map["canviewassignments"]
+        courseDescription <- map["coursedescription"]
+        cumGrade <- map["cumgrade"]
+        currentTerm <- map["currentterm"]
+        gradeBookCumGpa <- map["gradebookcumgpa"]
+        groupOwnerEmail <- map["groupowneremail"]
+        groupOwnerName <- map["groupownername"]
+        groupOwnerPhoto <- map["groupownerphoto"]
+        leadSectionId <- map["leadsectionid"]
+        markingPeriodId <- map["markingperiodid"]
+        mostRecentGroupPhoto <- map["mostrecentgroupphoto"]
+        publishGroupToUser <- map["publishgrouptouser"]
+        room <- map["room"]
+        schoolLevel <- map["schoollevel"]
+        sectionId <- map["sectionid"]
+        sectionIdentifier <- map["sectionidentifier"]
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Cranbrook Kingswood/Pods/Target Support Files/Pods-Cranbrook Kingswood/Pods-Cranbrook Kingswood-acknowledgements.markdown
+++ Cranbrook Kingswood/Pods/Target Support Files/Pods-Cranbrook Kingswood/Pods-Cranbrook Kingswood-acknowledgements.markdown
@@ -24,11 +24,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 
-## BTNavigationDropdownMenu
+## AlamofireObjectMapper
 
 The MIT License (MIT)
 
-Copyright (c) 2015 Pham Ba Tho
+Copyright (c) 2015 Tristan Himmelman
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
