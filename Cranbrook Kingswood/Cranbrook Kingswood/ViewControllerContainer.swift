b1264c044f17376ac62ab0732f801b25cb9fcc81
Stefan DeClerck <stefan.declerck26@gmail.com>
2016-08-09 21:45:42 -0700

    tab bar selection and design fully functional

--- Cranbrook Kingswood/Cranbrook Kingswood/ViewControllerContainer.swift
+++ Cranbrook Kingswood/Cranbrook Kingswood/ViewControllerContainer.swift
@@ -20,15 +20,27 @@ enum SelectedTabOptions {
 
 class ViewControllerContainer: UIViewController {
 
+    // View controllers
     @IBOutlet weak var ClassesViewController: UIView!
     @IBOutlet weak var AssignmentsViewController: UIView!
     @IBOutlet weak var DirectorySearchViewController: UIView!
     @IBOutlet weak var SettingsViewController: UIView!
     
+    // Custom tab bar buttons
+    @IBOutlet weak var tabClassButton: UIButton!
+    @IBOutlet weak var tabAssignmentsButton: UIButton!
+    @IBOutlet weak var tabSearchButton: UIButton!
+    @IBOutlet weak var tabSettingsButton: UIButton!
+    
+    // Custom tab bar icons
+    @IBOutlet weak var tabClassIcon: UIImageView!
+    @IBOutlet weak var tabAssignmentsIcon: UIImageView!
+    @IBOutlet weak var tabSearchIcon: UIImageView!
+    @IBOutlet weak var tabSettingsIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "Avenir Next", size: 20)!]
-        setSelectedTab(selectedTab)
+        setupViews()
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -36,7 +48,36 @@ class ViewControllerContainer: UIViewController {
         
     }
     
+    func setupViews() {
+        self.navigationController!.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "Avenir Next", size: 20)!]
+        setSelectedTab(selectedTab)
+        setupButtons()
+    }
+    
+    func setupButtons() {
+        tabClassButton.setBackgroundImage(Color.imageWithColor(UIColor.clearColor()), forState: .Highlighted)
+        tabAssignmentsButton.setBackgroundImage(Color.imageWithColor(UIColor.clearColor()), forState: .Highlighted)
+        tabSearchButton.setBackgroundImage(Color.imageWithColor(UIColor.clearColor()), forState: .Highlighted)
+        tabSettingsButton.setBackgroundImage(Color.imageWithColor(UIColor.clearColor()), forState: .Highlighted)
+    }
+    
+    @IBAction func tabSelected(sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            setSelectedTab(.ClassesTab)
+        case 1:
+            setSelectedTab(.AssignmentsTab)
+        case 2:
+            setSelectedTab(.DirectorySearchTab)
+        case 3:
+            setSelectedTab(.SettingsTab)
+        default:
+            setSelectedTab(.ClassesTab)
+        }
+    }
+    
     func setSelectedTab(tabID: SelectedTabOptions) {
+        setTabIcon(tabID)
         
         switch tabID {
         case .ClassesTab:
@@ -54,6 +95,37 @@ class ViewControllerContainer: UIViewController {
         case .SettingsTab:
             self.title = "Settings"
             hideTab(true, hideAssignments: true, hideSearch: true, hideSettings: false)
+            
+        }
+        
+    }
+    
+    func setTabIcon(tabID: SelectedTabOptions) {
+        
+        switch tabID {
+        case .ClassesTab:
+            tabClassIcon.image = UIImage(named: "class_list_tab_icon_selected")
+            tabAssignmentsIcon.image = UIImage(named: "assignment_tab_icon")
+            tabSearchIcon.image = UIImage(named: "student_search_tab_icon")
+            tabSettingsIcon.image = UIImage(named: "settings_tab_icon")
+            
+        case .AssignmentsTab:
+            tabClassIcon.image = UIImage(named: "class_list_tab_icon")
+            tabAssignmentsIcon.image = UIImage(named: "assignment_tab_icon_selected")
+            tabSearchIcon.image = UIImage(named: "student_search_tab_icon")
+            tabSettingsIcon.image = UIImage(named: "settings_tab_icon")
+            
+        case .DirectorySearchTab:
+            tabClassIcon.image = UIImage(named: "class_list_tab_icon")
+            tabAssignmentsIcon.image = UIImage(named: "assignment_tab_icon")
+            tabSearchIcon.image = UIImage(named: "student_search_tab_icon_selected")
+            tabSettingsIcon.image = UIImage(named: "settings_tab_icon")
+            
+        case .SettingsTab:
+            tabClassIcon.image = UIImage(named: "class_list_tab_icon")
+            tabAssignmentsIcon.image = UIImage(named: "assignment_tab_icon")
+            tabSearchIcon.image = UIImage(named: "student_search_tab_icon")
+            tabSettingsIcon.image = UIImage(named: "settings_tab_icon_selected")
             
         }
         
