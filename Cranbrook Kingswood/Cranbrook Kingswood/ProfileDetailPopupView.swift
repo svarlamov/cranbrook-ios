ec562b449b6f584b4bbe76f70f4b185fffb995ca
Stefan DeClerck <stefan.declerck26@gmail.com>
2016-08-25 21:59:12 -0700

    created profile detail popup view

---
+++ Cranbrook Kingswood/Cranbrook Kingswood/ProfileDetailPopupView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileDetailPopupView.swift
+//  Cranbrook Kingswood
+//
+//  Created by Stefan DeClerck on 8/25/16.
+//  Copyright © 2016 stefandeclerck. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftyJSON
+import Unbox
+import Alamofire
+import SwiftMessages
+
+class ProfileDetailPopupView: MessageView {
+    
+}
