a127a4fb7a51eb55c44e515c7b506d0d214627c9
Stefan DeClerck <stefan.declerck26@gmail.com>
2016-08-27 14:08:11 -0700

    made custom date scroll picker view

--- Cranbrook Kingswood/Cranbrook Kingswood/AssignmentsViewController.swift
+++ Cranbrook Kingswood/Cranbrook Kingswood/AssignmentsViewController.swift
@@ -11,16 +11,41 @@ import Alamofire
 import SwiftyJSON
 import Unbox
 
-class AssignmentsViewController: UIViewController {
-
+class AssignmentsViewController: UIViewController, AKPickerViewDataSource, AKPickerViewDelegate {
+    
+    @IBOutlet var pickerView: AKPickerView!
+    
+    let titles = ["Tokyo", "Kanagawa", "Osaka", "Aichi", "Saitama", "Chiba", "Hyogo", "Hokkaido", "Fukuoka", "Shizuoka"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
         
+        self.pickerView.font = UIFont(name: "HelveticaNeue-Light", size: 20)!
+        self.pickerView.highlightedFont = UIFont(name: "HelveticaNeue", size: 20)!
+        self.pickerView.pickerViewStyle = .Wheel
+        self.pickerView.maskDisabled = false
+        self.pickerView.reloadData()
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(true)
+    func numberOfItemsInPickerView(pickerView: AKPickerView) -> Int {
+        return self.titles.count
     }
+    
+    func pickerView(pickerView: AKPickerView, titleForItem item: Int) -> String {
+        return self.titles[item]
+    }
+    
+    func pickerView(pickerView: AKPickerView, imageForItem item: Int) -> UIImage {
+        return UIImage(named: self.titles[item])!
+    }
+    
+    func pickerView(pickerView: AKPickerView, didSelectItem item: Int) {
+        print("Your favorite city is \(self.titles[item])")
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {}
     
 }
 
--- Cranbrook Kingswood/Pods/Target Support Files/Pods-Cranbrook Kingswood/Pods-Cranbrook Kingswood-frameworks.sh
+++ Cranbrook Kingswood/Pods/Target Support Files/Pods-Cranbrook Kingswood/Pods-Cranbrook Kingswood-frameworks.sh
@@ -84,13 +84,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AKPickerView-Swift/AKPickerView_Swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Unbox/Unbox.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/AKPickerView-Swift/AKPickerView_Swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Unbox/Unbox.framework"
