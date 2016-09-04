afc0aae50918f8585a54f3e8dd513a539c2167f9
Stefan DeClerck <stefan.declerck26@gmail.com>
2016-09-03 20:43:15 -0400

    added calendar bar button item

--- Cranbrook Kingswood/Cranbrook Kingswood/AppDelegate.swift
+++ Cranbrook Kingswood/Cranbrook Kingswood/AppDelegate.swift
@@ -14,7 +14,7 @@ import Crashlytics
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? 
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Fabric.with([Crashlytics.self])
